245041520d834459e6d01167329d358d6a1ee782
rodrigofariasm <rodrigohfarias@gmail.com>
2015-10-17 18:27:11 +0100

    adding Login

---
+++ app/src/main/java/com/g14/ucd/fitassistant/Installation.java
@@ -0,0 +1,28 @@
+package com.g14.ucd.fitassistant;
+
+import com.parse.ParsePush;
+import android.util.Log;
+import com.parse.*;
+/**
+ * Created by rodrigofarias on 10/17/15.
+ */
+public class Installation {
+
+    public Installation(){
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
+    }
+
+    public void install(){
+        ParseInstallation.getCurrentInstallation().put("user", ParseUser.getCurrentUser());
+    }
+
+}
---
+++ app/src/main/java/com/g14/ucd/fitassistant/WelcomeActivity.java
@@ -0,0 +1,38 @@
+package com.g14.ucd.fitassistant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import static com.g14.ucd.fitassistant.R.*;
+
+/**
+ * Created by Rodrigo on 17/10/2015.
+ */
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        // Log in button click handler
+        Button loginButton = (Button) findViewById(id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Starts an intent of the log in activity
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
+        // Sign up button click handler
+        Button signupButton = (Button) findViewById(id.signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Starts an intent for the sign up activity
+                startActivity(new Intent(WelcomeActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+}
