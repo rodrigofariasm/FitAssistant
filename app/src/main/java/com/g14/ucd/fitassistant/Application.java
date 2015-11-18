1d4a61491be16e0053a9a43980f43f35125acc03
Leticia Barbosa <leticiabarbosa94@gmail.com>
2015-11-18 00:48:56 +0000

    removing weird chars from application and fixing newexercise activity

--- app/src/main/java/com/g14/ucd/fitassistant/Application.java
+++ app/src/main/java/com/g14/ucd/fitassistant/Application.java
@@ -5,13 +5,10 @@ import android.content.SharedPreferences;
 
 import com.g14.ucd.fitassistant.models.Day;
 import com.g14.ucd.fitassistant.models.Diet;
-<<<<<<< HEAD
 import com.g14.ucd.fitassistant.models.Exercise;
 import com.g14.ucd.fitassistant.models.FitActivity;
 import com.g14.ucd.fitassistant.models.Gym;
-=======
 import com.g14.ucd.fitassistant.models.Goal;
->>>>>>> 097de5aab3b00f9bc187c75130d5b91c9b123d8f
 import com.g14.ucd.fitassistant.models.Meal;
 import com.g14.ucd.fitassistant.models.Other;
 import com.parse.Parse;
